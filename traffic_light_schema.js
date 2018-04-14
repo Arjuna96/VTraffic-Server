1b9e29a4f4c28861afa940ad3b3911191bb32ae0
Arjuna96 <shanilarjuna@gmail.com>
2018-04-14 21:32:14 +0530

    traffic light schema code refactor

--- traffic_light_schema.js
+++ traffic_light_schema.js
@@ -2,11 +2,6 @@ var mongoose = require('mongoose');
 var assert = require('assert')
 var Schema = mongoose.Schema ; 
 var TrafficLightSchema = new Schema ({  
-    // _id : {
-    //     type:Number,
-    //     require: true
-    // },
-
     gpsLocation: {
         type:String,
         require: true, 
